ed54cd0877e1c25e93413c7dfbdbd78ac40bd026
Takeharu KATO <tkato1219@gmail.com>
2023-02-23 23:47:44 +0900

    修正概要: ディスクイメージロードエラーを報告するように修正, メモリリークを修正。

---
+++ include/misc.h
@@ -0,0 +1,13 @@
+/*
+ * SWORD Emulator
+ * Miscellaneous functions
+ */
+
+#ifndef	_MISC_H_
+#define	_MISC_H_
+
+#include "config.h"
+
+int check_file_exists(const char *_path, int _flags);
+
+#endif  /*  _MISC_H_  */
