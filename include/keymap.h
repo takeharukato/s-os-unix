2c2ace1bf9d36edbd7df073a7ae6d3d198c35010
Takeharu KATO <tkato1219@gmail.com>
2023-03-23 00:53:20 +0900

    修正概要: キーマップ関数定義構造体を修正し, 入力キーからSWORDのキャラクタコードに変換先コードを参照できるようにした。

--- include/keymap.h
+++ include/keymap.h
@@ -1,4 +1,31 @@
+/*
+   SWORD Emulator keymap header
+*/
+
 #ifndef _KEYMAP_H_
 #define _KEYMAP_H_
+
+/*
+ * keymap search key strings
+ */
+#define KEYMAP_NAME_BACKSPACE "backspace"
+#define KEYMAP_NAME_DELETE    "delete"
+#define KEYMAP_NAME_BEGIN     "begin"
+#define KEYMAP_NAME_END       "end"
+#define KEYMAP_NAME_UP        "up"
+#define KEYMAP_NAME_DOWN      "down"
+#define KEYMAP_NAME_FWD       "forward"
+#define KEYMAP_NAME_BACK      "back"
+#define KEYMAP_NAME_REDRAW    "redraw"
+#define KEYMAP_NAME_KILL      "kill"
+#define KEYMAP_NAME_TAB       "tab"
+#define KEYMAP_NAME_YANK      "yank"
+#define KEYMAP_NAME_IMODE     "imode"
+#define KEYMAP_NAME_CLEAR     "clear"
+#define KEYMAP_NAME_BREAK     "break"
+
+/*
+ * proto types
+ */
 void setdefaultkeymap(void);
-#endif
+#endif  /*  _KEYMAP_H_  */
