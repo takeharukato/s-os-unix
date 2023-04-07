1a289ebb772c9d0232658f686254a975946e1458
Takeharu KATO <tkato1219@gmail.com>
2023-04-07 21:32:32 +0900

    修正概要: スクリプトの配置先ディレクトリを取得するように修正。

--- test/run.sh
+++ test/run.sh
@@ -1,25 +1,25 @@
-#!/bin/sh
-DATA_DIR=.
+#!/bin/bash
+TEST_DIR=$(cd $(dirname $0);pwd)
 
 prepare_data(){
 
-    cp ${DATA_DIR}/back-sword-test.2d ${DATA_DIR}/sword-test.2d
-    cp ${DATA_DIR}/back-rw-test.2d ${DATA_DIR}/rw-test.2d
+    cp ${TEST_DIR}/back-sword-test.2d ${TEST_DIR}/sword-test.2d
+    cp ${TEST_DIR}/back-rw-test.2d ${TEST_DIR}/rw-test.2d
 }
 
 prepare_data
-./tst-swd-fs1.out ${DATA_DIR}/sword-test.2d
+${TEST_DIR}/tst-swd-fs1.out ${TEST_DIR}/sword-test.2d
 
 prepare_data
-./tst-swd-fs2.out ${DATA_DIR}/sword-test.2d
+${TEST_DIR}/tst-swd-fs2.out ${TEST_DIR}/sword-test.2d
 
 prepare_data
-./tst-swd-fs3.out ${DATA_DIR}/rw-test.2d
+${TEST_DIR}/tst-swd-fs3.out ${TEST_DIR}/rw-test.2d
 
 prepare_data
-./tst-swd-fs4.out ${DATA_DIR}/rw-test.2d
+${TEST_DIR}/tst-swd-fs4.out ${TEST_DIR}/rw-test.2d
 
 prepare_data
-./tst-swd-fat.out
+${TEST_DIR}/tst-swd-fat.out
 
 prepare_data
